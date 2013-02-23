a42337e4402851ffd72762d710331bdac9457335
Sushil Dahal <sushilthe@gmail.com>
2013-02-23 15:59:49 +0545

    Minor Fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Test App"
+    base_title = "Track Data"
     if page_title.empty?
       base_title
     else
